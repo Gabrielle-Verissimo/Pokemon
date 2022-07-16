4ba143adc526ace48e7dcbd7bff514d5b6555baf
Gabrielle Veríssimo <gabriellev007@gmail.com>
2022-07-15 21:12:03 -0300

    Atualização das falas, inserção da escolha de pronomes, inicio da animação da pokebola

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,7 +1,38 @@
+import { scripts } from "./script.js";
+
 const fala = document.querySelector('.balao-fala > p');
+let name = false;
+let pronums = false;
+let counter = 0;
 
 document.addEventListener('keyup', (e) => {
     if(e.key === 'Enter'){
-        fala.innerHTML = '';
+        if(pronums === false && counter === 6){
+            fala.innerHTML = 'vai';
+            pronums = true;
+        }
+
+        counter++;
+        if(counter === 3){
+            const pokebola = document.createElement('div');
+            const profCarvalho = document.querySelector('#prof-carvalho');
+            pokebola.classList = 'pokebola';
+            profCarvalho.appendChild(pokebola);
+        }
+        if(counter === 4){
+            const pokebola = document.querySelector('.pokebola');
+            // pokebola.style.backgroundImage = `url('./assets/img/pokebola-meio-aberta.png')`;
+            pokebola.classList.add('animation');
+        }
+        scripts.forEach(item => {
+            if(counter === item.id){
+                fala.innerHTML = `${item.text}`;
+            }
+        })
     }
 })
+
+// function chooseName(){
+//     const menuName = document.createElement('div');
+
+// }
---
+++ assets/scripts/script.js
@@ -0,0 +1,16 @@
+export const scripts = [
+    {id: 1, text: `Seja bem vindo ao mundo Pokémon!`},
+    {id: 2, text: `Meu nome é Samuel Carvalho.
+    As pessoas costumam me chamar de professor Carvalho.`},
+    {id: 3, text: `Esse mundo...`},
+    {id: 4, text: `...é habitado em toda parte por criaturas chamadas de Pokémon.`},
+    {id: 5, text: `Algumas pessoas veem Pokémons como pets.
+    Outras usam eles para rinha de bicho.
+    Enquanto a mim, tenho como profissão estudá-los.`},
+    {id: 6, text: `Mas primeiro, me fale um pouco sobre você.
+    Quais são seus pronomes?`}
+];
+
+
+
+// Vamos começar com seu nome. Como se chama?
