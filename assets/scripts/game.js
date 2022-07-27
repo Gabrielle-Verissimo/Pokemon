8fec30c78cd7755c073a960601daec38d8649a18
Gabrielle Veríssimo <gabriellev007@gmail.com>
2022-07-27 11:31:26 -0300

    Alteração geral do código. Inserção da tag canvas.

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -1,36 +1,45 @@
 import { scripts } from "./script.js";
 
-const fala = document.querySelector('.balao-fala > p');
-let name = false;
-let pronums = false;
-let counter = 0;
 
-document.addEventListener('keyup', (e) => {
-    if(e.key === 'Enter'){
-        if(pronums === false && counter === 6){
-            fala.innerHTML = 'vai';
-            pronums = true;
-        }
+window.onload = function() {
+    let c = document.querySelector("#game-board");
+    let ctx = c.getContext("2d");
+    let img = new Image();
+    img.src = 'fundo-prof-carvalho.png';
+    ctx.drawImage(img, 0, 600, img.width, img.height);
+};
 
-        counter++;
-        if(counter === 3){
-            const pokebola = document.createElement('div');
-            const profCarvalho = document.querySelector('#prof-carvalho');
-            pokebola.classList = 'pokebola';
-            profCarvalho.appendChild(pokebola);
-        }
-        if(counter === 4){
-            const pokebola = document.querySelector('.pokebola');
-            // pokebola.style.backgroundImage = `url('./assets/img/pokebola-meio-aberta.png')`;
-            pokebola.classList.add('animation');
-        }
-        scripts.forEach(item => {
-            if(counter === item.id){
-                fala.innerHTML = `${item.text}`;
-            }
-        })
-    }
-})
+// const fala = document.querySelector('.balao-fala > p');
+// let name = false;
+// let pronums = false;
+// let counter = 0;
+
+// document.addEventListener('keyup', (e) => {
+//     if(e.key === 'Enter'){
+//         if(pronums === false && counter === 6){
+//             fala.innerHTML = 'vai';
+//             pronums = true;
+//         }
+
+//         counter++;
+//         if(counter === 3){
+//             const pokebola = document.createElement('div');
+//             const profCarvalho = document.querySelector('#prof-carvalho');
+//             pokebola.classList = 'pokebola';
+//             profCarvalho.appendChild(pokebola);
+//         }
+//         if(counter === 4){
+//             const pokebola = document.querySelector('.pokebola');
+//             // pokebola.style.backgroundImage = `url('./assets/img/pokebola-meio-aberta.png')`;
+//             pokebola.classList.add('animation');
+//         }
+//         scripts.forEach(item => {
+//             if(counter === item.id){
+//                 fala.innerHTML = `${item.text}`;
+//             }
+//         })
+//     }
+// })
 
 // function chooseName(){
 //     const menuName = document.createElement('div');
